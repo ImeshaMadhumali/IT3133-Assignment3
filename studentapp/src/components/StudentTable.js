146f08daf07e78acedcb419c5cf44cdf18a179cc
Imesha Madhumali <139534200+ImeshaMadhumali@users.noreply.github.com>
2024-11-19 17:20:40 +0000

    Update the font size

--- studentapp/src/components/StudentTable.js
+++ studentapp/src/components/StudentTable.js
@@ -1,12 +1,27 @@
+import React from 'react'
 import '../assests/css/compo.css';
 import Profile from './Profile';
 import {useState} from "react";
 
 export default function StudentTable(props){
     const [stu,setStu]=useState(props.students[0]);
+    const [fontSize, setFontSize] = useState('medium');
+
+    const handleFontSizeChange = (size) => {
+      setFontSize(size);
+    };
+    
     return(
         <div className="outerDiv">
             <div className="leftDiv">
+            <div className={`font-${fontSize}`}>
+    <div>
+      <h6> Font Size  </h6>
+      <button onClick={() => handleFontSizeChange('small')}>S</button>
+      <button onClick={() => handleFontSizeChange('medium')}>M</button>
+      <button onClick={() => handleFontSizeChange('large')}>L</button>
+    </div> 
+    </div>   
                 <table border="1">
                     <thead>
                         <tr>
